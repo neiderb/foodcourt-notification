72299b111e165845a429036c6b118c047a7e354f
neiderb <neider.bustos@pragma.com.co>
2025-11-14 10:54:51 -0500

    add: Add validation messages for phone number and secure PIN in NotifyCompletedOrderRequest

--- src/main/java/com/foodcourt/notification/application/dto/request/NotifyCompletedOrderRequest.java
+++ src/main/java/com/foodcourt/notification/application/dto/request/NotifyCompletedOrderRequest.java
@@ -1,6 +1,16 @@
 package com.foodcourt.notification.application.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import static com.foodcourt.notification.domain.constants.NotificationValidationMessage.PHONE_NUMBER_IS_REQUIRED;
+import static com.foodcourt.notification.domain.constants.NotificationValidationMessage.SECURE_PIN_IS_REQUIRED;
+
 public record NotifyCompletedOrderRequest(
+	@NotNull(message = PHONE_NUMBER_IS_REQUIRED)
+	@NotBlank(message = PHONE_NUMBER_IS_REQUIRED)
 	String phoneNumber,
+	@NotNull(message = SECURE_PIN_IS_REQUIRED)
+	@NotBlank(message = SECURE_PIN_IS_REQUIRED)
 	String securePin
 ) {}
--- src/main/java/com/foodcourt/notification/domain/constants/NotificationValidationMessage.java
+++ src/main/java/com/foodcourt/notification/domain/constants/NotificationValidationMessage.java
@@ -5,6 +5,9 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class NotificationValidationMessage {
 	
+	public static final String PHONE_NUMBER_IS_REQUIRED = "Phone number is required";
+	public static final String SECURE_PIN_IS_REQUIRED = "Secure PIN is required";
+	
 	public static final String INVALID_PHONE_NUMBER = "Invalid phone number";
 	public static final String INVALID_SECURE_PIN = "Invalid secure PIN";
 	
